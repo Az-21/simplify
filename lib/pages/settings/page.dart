d7522cd286b3426ef9e2ec32ef2ae8b8b012f787
Az-21 <39803478+Az-21@users.noreply.github.com>
2022-08-03 16:19:23 +0530

    ✒️ added three case setting toggles

--- lib/pages/settings/page.dart
+++ lib/pages/settings/page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:simplify/components/drawer/drawer.dart';
 import 'package:simplify/components/drawer/logic/page_list.dart';
+import 'package:simplify/pages/settings/components/case.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({Key? key}) : super(key: key);
@@ -13,6 +14,18 @@ class SettingsPage extends StatelessWidget {
       drawer: const NavigationDrawer(root: PageList.Settings),
 
       /* --------------------------------- Content -------------------------------- */
+      body: ListView(
+        children: [
+          /* ----------------------------- Case Modifiers ----------------------------- */
+          Padding(
+            padding: const EdgeInsets.only(left: 16, top: 32),
+            child: Text("Case Modifiers", style: Theme.of(context).textTheme.headlineSmall),
+          ),
+          const SmartSentenceCaseSwitch(),
+          const HardSentenceCaseSwitch(),
+          const OptimizeArticlesSwitch(),
+        ],
+      ),
     );
   }
 }
